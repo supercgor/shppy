093d7a6af350a1b8d272951ff986b28ada6abf81
See.Looooo <siumabon123@gmail.com>
2025-01-22 12:06:47 +0800

    test

--- shppy/_core/hello.cpp
+++ shppy/_core/hello.cpp
@@ -1,17 +1,11 @@
 #include <cmath>
 
-#ifdef _WIN32
-    #define DLLEXPORT __declspec(dllexport)
-#else
-    #define DLLEXPORT
-#endif
-
 extern "C" {
-    DLLEXPORT float add_floats(float a, float b){
+    float add_floats(float a, float b){
         return a + b;
     }
 
-    DLLEXPORT void square_array(float* array, long size){
+    void square_array(float* array, long size){
         for(size_t i = 0; i < size; i++) {
             array[i] = array[i] * array[i];
         }
