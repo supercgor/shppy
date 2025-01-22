faf090c715b110ffb83cedfcfd6ae3beb048f0ba
See.Looooo <siumabon123@gmail.com>
2025-01-22 09:46:43 +0800

    test

--- shppy/_C.py
+++ shppy/_C.py
@@ -6,16 +6,19 @@ from ctypes import c_float, c_void_p, c_long
 from numpy.ctypeslib import load_library
 from typing import Callable
 
-try:
-    _lib = load_library(f"libshppy", os.path.abspath(__file__))
-except OSError:
-    raise OSError(f"No shared library found in {os.listdir(os.path.dirname(os.path.abspath(__file__)))}")
-
+if sys.platform.startswith("win"):
+    from ctypes import WinDLL
+    _lib = WinDLL(os.path.join(os.path.dirname(os.path.abspath(__file__)), "libshppy.dll"))
+else:
+    from ctypes import CDLL
+    if sys.platform.startswith("darwin"):
+        _lib = CDLL(os.path.join(os.path.dirname(os.path.abspath(__file__)), "libshppy.dylib"))
+    else:
+        _lib = CDLL(os.path.join(os.path.dirname(os.path.abspath(__file__)), "libshppy.so"))
 
 def add_floats(x: float, y: float) -> float: ...
 
 def square_array(arr: np.ndarray[float, np.dtype[np.float32]], n: int) -> None: ...
-
 
 _lib.add_floats.argtypes = [c_float, c_float]
 _lib.add_floats.restype = c_float
